219d5b13b182e907f2b445bf2ede306b494e1cc6
AJ Leonard <axlj24@gmail.com>
2018-03-28 22:30:24 -0400

    Added implementation

--- src/areLeafsEqual.js
+++ src/areLeafsEqual.js
@@ -1,5 +1,48 @@
-function leavesAreEqual(tree) {
-    return true;
+function evaluateTreeLeafs(tree) {
+    const keys = Object.keys(tree);
+    if (keys.length === 0) return undefined;
+
+    let leafValue;
+    let leafsAreEqual = true;
+
+    keys.forEach((key) => {
+        itemValue = tree[key];
+
+        if (typeof (itemValue) === "object" && itemValue !== null) {
+            const branchResult = evaluateTreeLeafs(itemValue);
+
+            if (branchResult === undefined) return true; // ignore branch & continue
+
+            if (branchResult.leafsAreEqual) {
+                if (leafValue === undefined) {
+                    leafValue = branchResult.leafValue;
+                } else {
+                    if (leafValue !== branchResult.leafValue) {
+                        leafsAreEqual = false;
+                        return false;
+                    }
+                }
+            } else {
+                leafsAreEqual = false;
+                return false;
+            }
+
+        } else {
+            if (leafValue === undefined) {
+                leafValue = itemValue;
+            } else {
+                if (itemValue !== leafValue) {
+                    leafsAreEqual = false;
+                    return false;
+                }
+            }
+        }
+    });
+
+    return {
+        leafsAreEqual,
+        leafValue
+    };
 }
 
-module.exports = leavesAreEqual;
+module.exports = evaluateTreeLeafs;
--- test/areLeafsEqual_tests.js
+++ test/areLeafsEqual_tests.js
@@ -1,8 +1,34 @@
 const sut = require('../src/areLeafsEqual');
 
+const passingSimpleTree = require('./test_data/passingSimpleTree');
+const passingComplexTree = require('./test_data/passingComplexTree');
+const failingSimpleTree = require('./test_data/failingSimpleTree');
+const failingComplexTree = require('./test_data/failingComplexTree');
+const emptyTree = {};
+
 describe('Tree Recursor', () => {
-    it('should return true when all simple leafs are equal', () => {
-        const result = sut();
-        expect(result).to.equal(true);
+    it('should return true when all simple tree leafs are equal', () => {
+        const result = sut(passingSimpleTree);
+        expect(result.leafsAreEqual).to.equal(true);
+    });
+
+    it('should return false when any simple tree leaf is different', () => {
+        const result = sut(failingSimpleTree);
+        expect(result.leafsAreEqual).to.equal(false);
+    });
+
+    it('should return true when every complex tree leaf is equal', () => {
+        const result = sut(passingComplexTree);
+        expect(result.leafsAreEqual).to.equal(true);
+    });
+
+    it('should return false when any complex tree leaf is different', () => {
+        const result = sut(failingComplexTree);
+        expect(result.leafsAreEqual).to.equal(false);
+    });
+
+    it('should return undefined if it has no children', () => {
+        const result = sut(emptyTree);
+        expect(result).to.be.undefined;
     });
 });
